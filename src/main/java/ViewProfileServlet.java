e02d330e918ef65fac719c2167390ecd7ab63fe3
Cole Usher <coleausher97@gmail.com>
2023-05-22 10:27:29 -0500

    sessions and cookies lecture

--- src/main/java/ViewProfileServlet.java
+++ src/main/java/ViewProfileServlet.java
@@ -3,11 +3,27 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/profile.jsp").forward(request, response);
+
+
+
+        // sessions and cookies lesson
+        HttpSession session = request.getSession();
+        session.setAttribute("username", "Dr. Codey McFly");
+
+
+        if(session.getAttribute("username") != null){
+            System.out.println("Session username already exists");
+        }else{
+            session.setAttribute("username", "Dr. Codey McFly");
+        }
+
+
+        request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }

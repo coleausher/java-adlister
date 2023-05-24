34a7eb8587ede2c941b3d6f4576731d3381e8c2d
Cole Usher <coleausher97@gmail.com>
2023-05-24 08:56:17 -0500

    JBDC exercise complete

--- src/main/java/dao/DaoFactory.java
+++ src/main/java/dao/DaoFactory.java
@@ -1,9 +1,13 @@
+package dao;
+
+import dao.MySQLAdsDao;
+
 public class DaoFactory {
     private static Ads adsDao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
-            adsDao = new ListAdsDao();
+            adsDao = new MySQLAdsDao(new Config());
         }
         return adsDao;
     }
---
+++ src/main/java/dao/MySQLAdsDao.java
@@ -0,0 +1,55 @@
+package dao;
+
+import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.jdbc.exceptions.MySQLTimeoutException;
+import models.Ad;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+    private Connection connection;
+
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(config.getUrl(), config.getUser(), config.getPassword());
+        } catch (Exception e) {
+            throw new RuntimeException("Error connecting to database");
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        try {
+            Statement stm = connection.createStatement();
+            String selectAds = "SELECT * FROM ads";
+            ResultSet rs = stm.executeQuery(selectAds);
+            List<Ad> allAds = new ArrayList<>();
+            while (rs.next()) {
+                allAds.add(new Ad(rs.getLong("id"), rs.getLong("user_id"), rs.getString("title"), rs.getString("description")));
+            }
+            return allAds;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating connection ");
+        }
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        try {
+            Statement stm = connection.createStatement();
+            String insertQuery = String.format("INSERT INTO ads (user_id, title, description) VALUES('%s', '%s', '%s')", ad.getUserId(), ad.getTitle(), ad.getDescription());
+            stm.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stm.getGeneratedKeys();
+            if (rs.next()) {
+                System.out.println("insert id " + rs.getLong(1));
+            }
+            return rs.getLong(1);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error adding ad");
+        }
+    }
+}

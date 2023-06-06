3044311f197eaaefbdd4b6abe85daa5b59f2f070
Cole Usher <coleausher97@gmail.com>
2023-06-06 08:41:37 -0500

    servlets exercise complete

---
+++ src/main/java/dao/Config.java
@@ -0,0 +1,16 @@
+package dao;
+
+public class Config {
+
+    public String getUrl(){
+        return "jdbc:mysql://localhost:3306/adlister_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+
+    public String getUser(){
+        return "user_db";
+    }
+
+    public String getPassword(){
+        return "codeup";
+    }
+}
